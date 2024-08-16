23eddbc3e58cf4422ea40379b0aa1898ac71adbd
Claudio Barsante <clbmribas@gmail.com>
2024-08-16 18:06:53 -0300

    refactor: date-helpers

---
+++ app/utils/date-helpers.ts
@@ -0,0 +1,58 @@
+import { format, parseISO } from 'date-fns';
+export function formatToISOWithoutMilliseconds(date: Date): string {
+    if (!date) return '';
+
+    try {
+        return (
+            date.getUTCFullYear() +
+            '-' +
+            String(date.getUTCMonth() + 1).padStart(2, '0') +
+            '-' +
+            String(date.getUTCDate()).padStart(2, '0') +
+            'T' +
+            String(date.getUTCHours()).padStart(2, '0') +
+            ':' +
+            String(date.getUTCMinutes()).padStart(2, '0') +
+            ':' +
+            String(date.getUTCSeconds()).padStart(2, '0') +
+            'Z'
+        );
+    } catch (error) {
+        return '';
+    }
+}
+
+export function getDateFromISODate(isoString: string): string {
+    try {
+        const date = parseISO(isoString);
+        const formattedDate = format(date, 'yyyy-MM-dd');
+
+        return formattedDate;
+    } catch (error) {
+        return isoString;
+    }
+}
+
+export function formatDayOfWeek(isoString: string): string {
+    if (!isoString) return '';
+
+    try {
+        const date = parseISO(isoString);
+        const formattedDate = format(date, 'EEE MMM dd');
+
+        return formattedDate;
+    } catch (error) {
+        return isoString;
+    }
+}
+
+export function getHourOfDay(isoString: string): string {
+    try {
+        const date = parseISO(isoString);
+        const formattedTime = format(date, 'h:mm aa');
+
+        return formattedTime;
+    } catch (error) {
+        return isoString;
+    }
+}
