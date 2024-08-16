0611ac624d1b2f4a57f68aba6bca71517523d710
Claudio Barsante <clbmribas@gmail.com>
2024-08-16 07:22:13 -0300

    refactor: add <OddCellContent/> to display odd cell content with status

---
+++ app/components/odd-cell-content.tsx
@@ -0,0 +1,29 @@
+import { Button } from '@/components/ui/button';
+import classnames from 'classnames';
+import { PiMedalFill } from 'react-icons/pi';
+import { formatOdds } from '../utils/format-odds';
+type Props = {
+    price: number | undefined;
+    maxPrice: number;
+};
+
+const OddCellContent = ({ price, maxPrice }: Props) => {
+    return (
+        <div className="relative">
+            <Button
+                variant="outline"
+                className={classnames('w-16 text-xs', {
+                    'cursor-not-allowed opacity-50': !price
+                })}
+                disabled={!price}
+            >
+                {(price && formatOdds(price)) || 'N/A'}{' '}
+                {price === maxPrice && (
+                    <PiMedalFill className="absolute right-1 top-1 text-green-500" />
+                )}
+            </Button>
+        </div>
+    );
+};
+
+export default OddCellContent;
--- app/components/table-body-odd-cells.tsx
+++ app/components/table-body-odd-cells.tsx
@@ -1,11 +1,10 @@
 import { Button } from '@/components/ui/button';
 import { TableCell } from '@/components/ui/table';
-import classnames from 'classnames';
-import { PiMedalFill } from 'react-icons/pi';
 import { bookmakerMap } from '../bookmaker-map';
 import { Game } from '../components/table-body';
 import { formatOdds } from '../utils/format-odds';
 import { getMaxOdds } from '../utils/get-max-odds';
+import OddCellContent from './odd-cell-content';
 
 type Props = {
     game: Game;
@@ -45,42 +44,14 @@ const TableBodyOddCells = ({ game }: Props) => {
             {cells.map((cell) => (
                 <TableCell key={cell.bookmakerKey}>
                     <div className="flex flex-col items-start gap-3">
-                        <div className="relative">
-                            <Button
-                                variant="outline"
-                                className={classnames('w-16 text-xs', {
-                                    'cursor-not-allowed opacity-50':
-                                        !cell.homeTeamPrice
-                                })}
-                                disabled={!cell.homeTeamPrice}
-                            >
-                                {(cell.homeTeamPrice &&
-                                    formatOdds(cell.homeTeamPrice)) ||
-                                    'N/A'}
-                            </Button>
-                            {cell.homeTeamPrice === maxOddHomeTeam.price && (
-                                <PiMedalFill className="absolute right-1 top-1 text-green-500" />
-                            )}
-                        </div>
-                        <div className="relative">
-                            <Button
-                                variant="outline"
-                                className={classnames('w-16 text-xs', {
-                                    'cursor-not-allowed opacity-50':
-                                        !cell.homeTeamPrice
-                                })}
-                                disabled={!cell.homeTeamPrice}
-                            >
-                                {(cell.awayTeamPrice &&
-                                    formatOdds(cell.awayTeamPrice)) ||
-                                    'N/A'}{' '}
-                                {cell.awayTeamPrice ===
-                                    maxOddAwayTeam.price && (
-                                    <PiMedalFill className="absolute right-1 top-1 text-green-500" />
-                                )}
-                            </Button>
-                            {/* <span>{cell.awayTeamPrice || 'N/A'}</span> */}
-                        </div>
+                        <OddCellContent
+                            price={cell.homeTeamPrice}
+                            maxPrice={maxOddHomeTeam.price}
+                        />
+                        <OddCellContent
+                            price={cell.awayTeamPrice}
+                            maxPrice={maxOddAwayTeam.price}
+                        />
                     </div>
                 </TableCell>
             ))}
